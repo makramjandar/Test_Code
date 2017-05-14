877b8ac281f741e1a8ddcffaf5cc8509ef2525ab
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-05-13 20:14:41 -0400

    Edit fizzBuzz.py and tests

--- Problems/fizzBuzz.py
+++ Problems/fizzBuzz.py
@@ -13,34 +13,59 @@ def main():
     except ValueError:
         print('PASSED ValueError check')
 
-    print('FizzBuzz 15:')
-    fizz_buzz(15)
+    test_fizz_buzz = [
+        '1',
+        '2',
+        'Fizz',
+        '4',
+        'Buzz',
+        'Fizz',
+        '7',
+        '8',
+        'Fizz',
+        'Buzz',
+        '11',
+        'Fizz',
+        '13',
+        '14',
+        'FizzBuzz'
+    ]
+
+    result = fizz_buzz(15)
+    # print('fizz_buzz(15): {}'.format(result))
+    for i, n in enumerate(test_fizz_buzz):
+        if n != result[i]:
+            print('FAILED fizz_buzz test')
+            return -1
+    print('PASSED fizz_buzz test')
 
     return 0
+
 
 def fizz_buzz(n):
     ''' Classic FizzBuzz implementation.
         Multiples of 3 => 'Fizz'
         Multiples of 5 => 'Buzz'
-        Multiples of 3 and 5 => 'FizzBuzz'
+        Multiples of both 3 and 5 => 'FizzBuzz'
     '''
     # Check input
     if type(n) is not int:
         raise TypeError
-    if n <= 0:
+    if n < 1:
         raise ValueError
 
+    fizzy = []
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            print('FizzBuzz')
+            fizzy.append('FizzBuzz')
         elif i % 3 == 0:
-            print('Fizz')
+            fizzy.append('Fizz')
         elif i % 5 == 0:
-            print('Buzz')
+            fizzy.append('Buzz')
         else:
-            print(str(i))
+            fizzy.append(str(i))
 
-    return 0
+    return fizzy
 
 if __name__ == '__main__':
     main()
