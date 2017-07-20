5cf8043929cc9c70ff06038b44ab2a6e301f2c62
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-07-19 22:01:23 -0400

    Add digital root solution to add digits problem

--- Problems/addDigits.py
+++ Problems/addDigits.py
@@ -12,6 +12,7 @@ def main():
         [65536, 7]
     ]
 
+    print('Testing add_digits')
     for item in tests:
         try:
             temp_result = add_digits(item[0])
@@ -19,6 +20,22 @@ def main():
                 print('PASSED: add_digits({}) returned {}'.format(item[0], temp_result))
             else:
                 print('FAILED: add_digits({}) returned {}, should have returned {}'.format(item[0], temp_result, item[1]))
+
+        except TypeError:
+            print('PASSED TypeError test')
+
+        except ValueError:
+            print('PASSED ValueError test')
+
+
+    print('\nTesting add_digits_digital_root')
+    for item in tests:
+        try:
+            temp_result = add_digits_digital_root(item[0])
+            if temp_result == item[1]:
+                print('PASSED: add_digits_digital_root({}) returned {}'.format(item[0], temp_result))
+            else:
+                print('FAILED: add_digits_digital_root({}) returned {}, should have returned {}'.format(item[0], temp_result, item[1]))
 
         except TypeError:
             print('PASSED TypeError test')
@@ -55,6 +72,27 @@ def add_digits(val):
         digit_sum = temp_sum
 
     return digit_sum
+
+
+def add_digits_digital_root(val):
+    '''
+    This is a digital root, can be solved taking % 9
+    Time: O(1)
+    Complexity: O(1)
+    '''
+    # Check inputs
+    if type(val) is not int:
+        raise TypeError('Input must be an integer')
+
+    if val < 0:
+        raise ValueError('Input must be non-negative')
+
+    if val == 0:
+        return 0
+    elif val % 9 == 0:
+        return 9
+    else:
+        return val % 9
 
 
 if __name__ == '__main__':
