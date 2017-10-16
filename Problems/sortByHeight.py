90169095a9e1adbc23e1efa35ea0e1a9a09259de
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-10-16 18:50:42 -0400

    Solve Code Fights sortByHeight problem

---
+++ Problems/sortByHeight.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+#  Code Fights Arcade Mode
+
+
+def sortByHeight(a):
+    trees = [i for i, t in enumerate(a) if t == -1]
+    humans = sorted([h for h in a if h != -1])
+    for tree in trees:
+        humans.insert(tree, -1)
+    return humans
+
+
+def main():
+    a = [-1, 150, 190, 170, -1, -1, 160, 180]
+    new = sortByHeight(a)
+    print(new)
+
+
+if __name__ == '__main__':
+    main()
