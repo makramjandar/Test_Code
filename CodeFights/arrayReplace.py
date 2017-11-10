23e75c71fd819f4d5aef4eff59b3c9fcf65b6339
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-11-09 20:52:03 -0500

    Set up Code Fights array replace problem

---
+++ CodeFights/arrayReplace.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+# Code Fights Add Border Problem
+
+
+def arrayReplace(inputArray, elemToReplace, substitutionElem):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
