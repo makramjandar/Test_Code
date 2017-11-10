e9e632008db1eb2bbdbd989584b82255a10f8944
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-11-09 21:10:05 -0500

    Solve Code Fights array replace problem

--- CodeFights/arrayReplace.py
+++ CodeFights/arrayReplace.py
@@ -3,11 +3,24 @@
 
 
 def arrayReplace(inputArray, elemToReplace, substitutionElem):
-    pass
+    return [x if x != elemToReplace else substitutionElem for x in inputArray]
 
 
 def main():
-    pass
+    tests = [
+        [[1, 2, 1], 1, 3, [3, 2, 3]],
+        [[1, 2, 3, 4, 5], 3, 0, [1, 2, 0, 4, 5]],
+        [[1, 1, 1], 1, 10, [10, 10, 10]]
+    ]
+
+    for t in tests:
+        res = arrayReplace(t[0], t[1], t[2])
+        if t[3] == res:
+            print("PASSED: arrayReplace({}, {}, {}) returned {}"
+                  .format(t[0], t[1], t[2], res))
+        else:
+            print("FAILED: arrayReplace({}, {}, {}) returned {}, should have returned {}"
+                  .format(t[0], t[1], t[2], res, t[3]))
 
 
 if __name__ == '__main__':
