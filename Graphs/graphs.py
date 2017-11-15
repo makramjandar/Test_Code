5ba8686698be9e336d5e99008582681ef56e3d5e
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-11-15 10:39:32 -0500

    Add Digraph class to graphs code

--- Graphs/graphs.py
+++ Graphs/graphs.py
@@ -7,7 +7,7 @@ class Node(object):
     """docstring for Node"""
 
     def __init__(self, name):
-        """ Assumes name is a string"""
+        """Assumes name is a string"""
         self.name = name
 
     def getName(self):
@@ -33,3 +33,42 @@ class Edge(object):
 
     def __str__(self):
         return self.src.getName() + '->' + self.dest.getName()
+
+
+class Digraph(object):
+    """docstring for Digraph"""
+
+    def __init__(self):
+        self.edges = {}
+
+    def addNode(self, node):
+        if node in self.edges:
+            raise ValueError('Duplicate node')
+        else:
+            self.edges[node] = []
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.edges and dest in self.edges):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+
+    def childrenOf(self, node):
+        return self.edges[node]
+
+    def hasNode(self, node):
+        return node in self.edges
+
+    def getNode(self, name):
+        for n in self.edges:
+            if n.getName() == name:
+                return n
+        raise NameError(name)
+
+    def __str__(self):
+        result = ''
+        for src in self.edges:
+            for dest in self.edges[src]:
+                result = result + src.getName() + '->' + dest.getName() + '\n'
+        return result[:-1]  # omit final newline
