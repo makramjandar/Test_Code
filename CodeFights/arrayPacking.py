47b4779b82035d0478985c85c3e7e95581ef8efe
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-12-12 14:42:39 -0500

    Solve Code Fights array packing problem

--- CodeFights/arrayPacking.py
+++ CodeFights/arrayPacking.py
@@ -1,15 +1,15 @@
 #!/usr/local/bin/python
-# Code Fights Are Equally Strong Problem
+# Code Fights Array Packing (Core) Problem
 
 
-def arrayPacking():
-    pass
+def arrayPacking(a):
+    return sum([n << 8*i for i, n in enumerate(a)])
 
 
 def main():
     tests = [
-        [],
-        []
+        [[24, 85, 0], 21784],
+        [[23, 45, 39], 2567447]
     ]
 
     for t in tests:
