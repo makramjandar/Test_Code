26749bd6bd36c4cd930e60c1eb2d0460fd16506e
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-12-27 09:01:51 -0500

    Add half tests to knapsack light problem

--- CodeFights/knapsackLight.py
+++ CodeFights/knapsackLight.py
@@ -13,6 +13,9 @@ def knapsackLight(value1, weight1, value2, weight2, maxW):
 def main():
     tests = [
         [10, 5, 6, 4, 8, 10],
+        [10, 5, 6, 4, 9, 16],
+        [5, 3, 7, 4, 6, 7],
+        [10, 2, 11, 3, 1, 0],
         []
     ]
 
