13706d50a7e84ec6ba3062931b5c873e19d5567a
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-01-27 11:40:47 -0500

    First solution to Code Fights is MAC48 address problem

---
+++ CodeFights/isMAC48Address.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python
+# Code Fights Is MAC48 Address Problem
+
+
+def isMAC48Address(inputString):
+    chunks = inputString.split("-")
+    if len(chunks) != 6:
+        return False
+    try:
+        for c in chunks:
+            int(c, 16)
+    except ValueError:
+        return False
+    return True
+
+
+def main():
+    tests = [
+        ["00-1B-63-84-45-E6", True],
+        ["Z1-1B-63-84-45-E6", False],
+        ["not a MAC-48 address", False],
+        ["FF-FF-FF-FF-FF-FF", True],
+        ["00-00-00-00-00-00", True],
+        ["G0-00-00-00-00-00", False],
+        ["02-03-04-05-06-07-", False],
+        ["12-34-56-78-9A-BC", True],
+        ["FF-FF-AB-CD-EA-BC", True],
+        ["12-34-56-78-9A-BG", False]
+    ]
+
+    for t in tests:
+        res = isMAC48Address(t[0])
+        ans = t[1]
+        if ans == res:
+            print("PASSED: isMAC48Address({}) returned {}"
+                  .format(t[0], res))
+        else:
+            print("FAILED: isMAC48Address({}) returned {}, answer: {}"
+                  .format(t[0], res, ans))
+
+
+if __name__ == '__main__':
+    main()
