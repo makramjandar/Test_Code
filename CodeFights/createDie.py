dab3b241552734d6810013f53d55c2fec3e1e512
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-03-13 10:00:12 -0400

    Set up Code Fights create die problem

---
+++ CodeFights/createDie.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python
+# Code Fights Create Die Problem
+
+import random
+
+
+def createDie(seed, n):
+    class Die(object):
+        pass
+
+    class Game(object):
+        die = Die(seed, n)
+
+    return Game.die
+
+
+def main():
+    tests = [
+        [37237, 5, 3],
+        [36706, 12, 9],
+        [21498, 10, 10],
+        [2998, 6, 3],
+        [5509, 10, 4]
+    ]
+
+    for t in tests:
+        res = createDie(t[0], t[1])
+        ans = t[2]
+        if ans == res:
+            print("PASSED: createDie({}, {}) returned {}"
+                  .format(t[0], t[1], res))
+        else:
+            print("FAILED: createDie({}, {}) returned {}, answer: {}"
+                  .format(t[0], t[1], res, ans))
+
+
+if __name__ == '__main__':
+    main()
