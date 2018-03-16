10a5129ed2ec38f85c55f84306b61648840721f2
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-03-16 15:39:29 -0400

    Clean up old code in isCoolTeam

--- CodeFights/isCoolTeam.py
+++ CodeFights/isCoolTeam.py
@@ -19,53 +19,6 @@ class Team(object):
         return len(last) <= 1
 
 
-class Team_OLD(object):
-    def __init__(self, names):
-        self.names = names
-
-    def __bool__(self, names):
-        choices = [(p[0].lower(), p[-1].lower()) for p in names]
-        print("People: {}".format(choices))
-
-        if len(names) <= 1:
-            return True
-        else:
-            lp = self.longest_path(choices)
-            print("Longest path: {}".format(lp))
-            print("Result is len(lp) ({}) == len(names) ({}) is {}"
-                  .format(len(lp), len(names), len(lp) == len(names)))
-            return len(lp) == len(names)
-
-    def get_neighbs(self, person, choices):
-        """
-        person: a tuple ("first letter", "last letter")
-        choices: a list of tuples in same format
-        Returns a list of tuples where choices start with the same letter
-            that person ended with
-        """
-        print("In get_neighbs, p: {}, choices: {}".format(person, choices))
-        if choices is None:
-            print("No choices?")
-            return []
-        return [p for p in choices if p[0] == person[-1]]
-
-    def longest_path_from(self, person, choices):
-        choices = choices.remove(person)
-        neighbors = self.get_neighbs(person, choices)
-
-        if neighbors:
-            paths = (self.longest_path_from(p, choices) for p in neighbors)
-            max_path = max(paths, key=len)
-        else:
-            max_path = []
-
-        return [person] + max_path
-
-    def longest_path(self, choices):
-        return max((self.longest_path_from(p, choices) for p in choices),
-                   key=len)
-
-
 def isCoolTeam(team):
     return bool(Team(team))
 
