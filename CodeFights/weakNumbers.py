260ac2d7cead3a362aec1e4ac9552ae106a341ec
AIe !! <makramjandar@gmail.com>
2019-12-11 01:43:45 +0100

    Update weakNumbers.py

--- CodeFights/weakNumbers.py
+++ CodeFights/weakNumbers.py
@@ -2,6 +2,34 @@
 # Code Fights Weak Numbers Problem
 
 
+x = 10 if a > b else 11
+
+
+def prime_factors(n):
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            n /= i
+            yield i
+        else:
+            i += 1
+
+    yield n if n > 1
+        
+
+     for i*i in range (2,n+1)
+        if n % i == 0:
+        
+         
+gen_exp = (x ** 2 while i * i <= n if n % i == 0 else i += 1 )
+
+
+list({(yield i) for i in range(3)})
+
+
+
+
+
 def weakNumbers(n):
     def get_divisors(n):
         divs = []
